9bf4c6bb5834fdae6dce5ac4fa7a43520d5f68e2
Le Nam <ltnam2804@gmail.com>
2018-04-01 13:49:05 +0700

    update usernamea

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -299,7 +299,7 @@
                 <img src="/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
                 </div>
                 <div class="pull-left info">
-                <p>Ken.Deveio</p>
+                <p>Ken.imediavn</p>
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
                 </div>
             </div>
